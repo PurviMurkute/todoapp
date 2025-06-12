02c85b60d88c5e95f2472100322d6f670be4e0f2
PurviMurkute <purvimurkute51@gmail.com>
2025-06-12 12:18:54 +0530

    task deletion performed

--- src/App.jsx
+++ src/App.jsx
@@ -1,85 +1,110 @@
 import { useEffect, useState } from "react";
 import addicon from "./assets/addicon.png";
-import todolisticon from './assets/todolisticon.png';
+import todolisticon from "./assets/todolisticon.png";
+import { ToDoCard } from "./ToDoCard";
+import tasksimg from "./assets/tasks.png";
+import toast, { Toaster } from "react-hot-toast";
 
 export const App = () => {
   const [newtask, setNewTask] = useState({
     task: "",
-    priority: ""
-  })
-  const [tasks, setTasks] = useState([
-    {
-      task: "task 1",
-      priority: "High"
-    },
-    {
-      task: "task 2",
-      priority: "Low"
-    },
-    {
-      task: "task 3",
-      priority: "Medium"
-    },
-    {
-      task: "task 3",
-      priority: "Medium"
-    },
-    {
-      task: "task 3",
-      priority: "Medium"
+    priority: "",
+  });
+  const [tasks, setTasks] = useState([]);
+
+  let setTaskinLS;
+
+  useEffect(() => {
+    if (tasks.length == 0) return;
+
+    setTaskinLS = localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
+
+  useEffect(() => {
+    if (setTaskinLS) {
+      const getTasksfromLS = JSON.parse(localStorage.getItem("tasks") || []);
+
+      setTasks(getTasksfromLS);
     }
-  ]);
+  }, []);
+
+  const deleteTask = (index) => {
+    const listAfterDeletion = tasks.filter((_, i) => i != index);
+
+    setTasks(listAfterDeletion);
+    toast.success("Task delete successfully");
+  };
 
   return (
     <div className="min-h-screen bg-teal-100">
-      <h1 className="flex justify-center text-teal-800 text-4xl font-bold mb-5 pt-5">ToDoApp<img src={todolisticon} alt="icon" className="h-[50px]"/> </h1>
+      <h1 className="flex justify-center text-teal-800 text-4xl font-bold mb-5 pt-5">
+        ToDoApp
+        <img src={todolisticon} alt="icon" className="h-[50px]" />{" "}
+      </h1>
       <div className="flex flex-row justify-center px-2">
         <input
           type="text"
           placeholder="Add a New Task..."
           value={newtask.task}
-          onChange={(e) => {setNewTask({
-            ...newtask,
-            task: e.target.value
-          })}}
-          className="w-1/2 md:w-1/3 border-2 border-gray-400 my-2 px-1 md:px-5 py-1 rounded-lg shadow-md focus:outline-none"
+          onChange={(e) => {
+            setNewTask({
+              ...newtask,
+              task: e.target.value,
+            });
+          }}
+          className="w-1/2 md:w-1/4 border-2 border-gray-400 my-2 px-1 md:px-5 py-1 rounded-lg shadow-md focus:outline-none"
         />
-        <select className="w-1/4 md:w-1/6 border-2 border-gray-400 my-2 px-1 md:px-5 py-1 rounded-lg shadow-md ms-2 focus:outline-none" value={newtask.priority} onChange={(e)=>{setNewTask({
-          ...newtask,
-          priority: e.target.value
-        })}}>
+        <select
+          className="w-1/4 md:w-1/6 border-2 border-gray-400 my-2 px-1 md:px-5 py-1 rounded-lg shadow-md ms-2 focus:outline-none"
+          value={newtask.priority}
+          onChange={(e) => {
+            setNewTask({
+              ...newtask,
+              priority: e.target.value,
+            });
+          }}
+        >
           <option value="">Select Priority</option>
-          <option value="High">High</option>
-          <option value="Medium">Medium</option>
-          <option value="Low">Low</option>
+          <option value="high">High</option>
+          <option value="medium">Medium</option>
+          <option value="low">Low</option>
         </select>
         <img
           src={addicon}
           alt="add-icon"
           className="h-[40px] md:h-[45px] ms-1 md:ms-2"
-          onClick={()=>{
+          onClick={() => {
+            if (!newtask.task || !newtask.priority) {
+              toast.error("Please enter task and select priority");
+              return;
+            }
             setTasks([...tasks, newtask]);
             setNewTask({
               task: "",
-              priority: ""
-            })
+              priority: "",
+            });
+            toast.success("Task added successfully");
           }}
         />
-       
       </div>
-      <div className="h-[450px] md:h-[500px] b-cyan-300 overflow-scroll mx-5 md:mx-50 mt-5 py-2 md:py-5 bg-emerald-500 shadow-2xl rounded-lg">
-          {
-            tasks.map((taskitem, i)=>{
-              const {task, priority} = taskitem;
-              return(
-                <div key={i} className="border-2 border-teal-600 py-2 px-7 my-5 block mx-auto w-[220px] md:w-1/2 rounded-xl shadow-lg text-teal-800 bg-teal-200" >
-                  <h2>{task}</h2>
-                  <p>{priority}</p>
-                </div>
-              )
-            })
-          }
+
+      <div className="h-[450px] w-[250px] md:w-[800px] md:h-[500px] b-cyan-300 overflow-y-scroll block mx-auto mt-5 py-2 md:py-5 bg-emerald-400 shadow-2xl rounded-lg">
+        <div>
+          {tasks.map((taskitem, index) => {
+            const { task, priority } = taskitem;
+            return (
+              <ToDoCard
+                task={task}
+                priority={priority}
+                key={index}
+                index={index}
+                deleteTask={deleteTask}
+              />
+            );
+          })}
         </div>
+      </div>
+      <Toaster />
     </div>
   );
 };
---
+++ src/ToDoCard.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import {  Trash2 } from 'lucide-react';
+
+export const ToDoCard = ({task, priority, deleteTask, index}) => {
+  const PRIORITY_CLASSES = {
+    high: "bg-teal-500 border-3",
+    medium: "bg-teal-400  border-2",
+    low: "bg-teal-300 border-1"
+  }
+
+  return (
+    <div
+      className={`border-2 py-2 px-4 my-5 border-white block mx-auto w-[220px] md:w-[600px] rounded-xl shadow-lg text-teal-800 relative ${PRIORITY_CLASSES[priority]}`}
+    >
+      <h2 className="font-bold text-lg">{task}</h2>
+      <p className="text-md">{priority}</p>
+      <Trash2 color="#124d59" className="position absolute top-5 right-2 w-[20px] hover:w-[22px]" onClick={()=>{deleteTask(index)}}/>
+    </div>
+  );
+};
